46f831676ba6646379c7baed1540e61bba58b524
JakobFrank <jakobfrank97@gmail.com>
2018-10-20 18:33:02 -0500

    Added punctuation parsing to processing.py

--- processing.py
+++ processing.py
@@ -1,4 +1,5 @@
 import dict
+import re
 
 def lookUp(word, age):
     """Returns a word, based on given word, corresponding to given age"""
@@ -6,8 +7,18 @@ def lookUp(word, age):
 
 def stringProcessor(text, age):
     """Returns processed text corresponding to given age"""
-    inarr = text.split()
+    inarr = re.findall(r"[\w']+|[.,!?;]", text)
     outarr = []
+    text = ""
     for word in inarr:
         outarr.append(lookUp(word, age))
-    return ' '.join(word for word in outarr).capitalize()
+    for word in outarr:
+        if (word != ',' and word != '.') and text != "":
+            text = text + ' ' + word
+        else:
+            text = text + word
+    x = text.capitalize()
+    return x
+
+print(stringProcessor("Wow, that's cool lmao.", 15))
+print(stringProcessor("lol, lmao, laughter.", 30))
