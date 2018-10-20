bcc520956aea5409e9a098a5d725556c70eac3a7
JakobFrank <jakobfrank97@gmail.com>
2018-10-20 15:00:36 -0500

    stringProcessor definition

--- data.py
+++ data.py
@@ -1,3 +1,11 @@
 def lookUp(word, age):
     """Returns a word, based on given word, corresponding to given age"""
     return word
+
+def stringProcessor(text, age):
+    """Returns processed text corresponding to given age"""
+    inarr = text.split()
+    outarr = []
+    for word in inarr:
+        outarr.append(lookUp(word, age))
+    return ' '.join(word for word in outarr)
