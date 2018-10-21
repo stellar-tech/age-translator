a4f60323848166987a87fbf2ac538a085bfdadf4
Ryan Hodin <ryan951357@gmail.com>
2018-10-20 20:25:25 -0500

    Implement baby talk for children under 3
    So we can have a smaller dictionary, along with more humorous behavior.

--- processing.py
+++ processing.py
@@ -1,8 +1,16 @@
 import dict
 import re
+import random
 
 def lookUp(word, age):
     """Returns a word, based on given word, corresponding to given age"""
+
+    # Handle small children
+    if age<3:
+        babyTalk = ["goo", "ga", "waa", "WAA", "waah"]
+        return babyTalk[random.randint(0, len(babyTalk)-1)]
+
+    # If 3 or older, use the actual translation dictionary
     return dict.dictLookup(word,age)
 
 def stringProcessor(text, age):
